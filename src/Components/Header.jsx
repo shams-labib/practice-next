d137551b54a9a465fbbd40036c2fd01e3962605d
shams al labib <samslabib19@gmail.com>
2026-03-11 00:45:23 +0600

    added

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from "react";
 
 const Header = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <div>
+      <h1>Hello</h1>
+    </div>
+  );
 };
 
 export default Header;
