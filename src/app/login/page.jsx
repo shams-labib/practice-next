990000b79b2da2a856a39b2a56f57a5475ad1772
shams al labib <samslabib19@gmail.com>
2026-03-11 01:38:36 +0600

    tast 1 successfully fucked

---
+++ src/app/login/page.jsx
@@ -0,0 +1,12 @@
+import Title from "@/Components/Title";
+import React from "react";
+
+const Login = () => {
+  return (
+    <div>
+      <Title>Here is the login page</Title>
+    </div>
+  );
+};
+
+export default Login;
---
+++ src/app/tutorials/page.jsx
@@ -0,0 +1,12 @@
+import Title from "@/Components/Title";
+import React from "react";
+
+const Tutorials = () => {
+  return (
+    <div>
+      <Title>This is Tutorials dudu</Title>
+    </div>
+  );
+};
+
+export default Tutorials;
